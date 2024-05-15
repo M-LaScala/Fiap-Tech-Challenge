14b3efd9189079acc2c567644f9f4a1ffd41243a
Matheus La Scala <matheus.lascala02@gmail.com>
2024-05-14 23:36:08 -0300

    Iniciando a criação de testes para o contato

---
+++ Tech.Challenge.Grupo27.Tests/Entidade/TesteContato.cs
@@ -0,0 +1,44 @@
+﻿using Tech.Challenge.Grupo27.Domain.Models.ContatoAggregate;
+using Tech.Challenge.Grupo27.Domain.Shared.ValueObject;
+
+namespace Tech.Challenge.Grupo27.Tests.Entidade
+{
+    public class TesteContato
+    {
+        /// <summary>
+        /// Criação de contato padrão.
+        /// </summary>
+        [Fact]
+        public void ValidaCriacaoContato()
+        {
+            Telefone telefone = new Telefone("13", "997843424");
+            Contato contato = new Contato("Pedro Luiz", "pedro_2004@gmail.com", telefone);
+
+            Assert.True(contato.Valid);
+        }
+
+        /// <summary>
+        /// Criação de contato com e-mail invalido.
+        /// </summary>
+        [Fact]
+        public void ValidaErroCriacaoContatoEmail()
+        {
+            Telefone telefone = new Telefone("13", "997843424");
+            Contato contato = new Contato("Pedro Luiz", "e-mail", telefone);
+
+            Assert.False(contato.Valid);
+        }
+
+        /// <summary>
+        /// Criação de contato com DDD invalido.
+        /// </summary>
+        [Fact]
+        public void ValidaErroCriacaoTelefoneDDD()
+        {
+            Telefone telefone = new Telefone("555", "983243424");
+            Contato contato = new Contato("Pedro Luiz", "pedro_2004@gmail.com", telefone);
+
+            Assert.False(contato.Valid);
+        }
+    }
+}
