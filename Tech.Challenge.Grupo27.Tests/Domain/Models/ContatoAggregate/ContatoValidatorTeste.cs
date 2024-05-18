e4359ea97b68c18e308944ad789be2201e285eca
Tiago <rm353730@fiap.com.br>
2024-05-18 15:00:16 -0300

    implementando teste unitário contatovalidator

---
+++ Tech.Challenge.Grupo27.Tests/Domain/Models/ContatoAggregate/ContatoValidatorTeste.cs
@@ -0,0 +1,71 @@
+﻿using Tech.Challenge.Grupo27.Domain.Models.ContatoAggregate;
+using Tech.Challenge.Grupo27.Domain.Shared.ValueObject;
+
+namespace Tech.Challenge.Grupo27.Tests.Domain.Models.ContatoAggregate
+{
+    public class ContatoValidatorTeste
+    {
+        /// <summary>
+        /// Contato Validator
+        /// </summary>
+        [Fact]
+        public void ContatoValidator_ContatoValido_DeveRetornarSucesso()
+        {
+            //Arrange
+            var contatoValidator = new ContatoValidator();
+
+            //Act
+            var contatoRsult = contatoValidator.Validate(ObterContatoMock("Mario José Silva","jm@gmail.com"));
+
+            //Assert
+            Assert.True(contatoRsult.IsValid);
+        }
+
+        /// <summary>
+        /// Contato Validator Nome Obrigatório
+        /// </summary>
+        [Fact]
+        public void ContatoValidator_NomeNull_DeveRetornarErro()
+        {
+            //Arrange
+            var contatoValidator = new ContatoValidator();
+
+            //Act
+            var contatoRsult = contatoValidator.Validate(ObterContatoMock(null,"ninguem@gmail.com"));
+
+            //Assert
+            Assert.False(contatoRsult.IsValid);
+            Assert.Contains("Nome é obrigatório.", contatoRsult.Errors.Select(e => e.ErrorMessage));
+        }
+
+        /// <summary>
+        /// Contato Validator Email Invalido
+        /// </summary>
+        [Theory]
+        [InlineData("joao.silva&.com")]
+        [InlineData("joao.br#.com.br")]
+        [InlineData("xxxxx.br")]
+        [InlineData("joao.br.com@")]
+        public void ContatoValidator_EmailInvalido_DeveRetornarErro(string email)
+        {
+            //Arrange
+            var contatoValidator = new ContatoValidator();            
+            
+            //Act
+            var contatoRsult = contatoValidator.Validate(ObterContatoMock("Marcos Silva", email));
+
+            //Assert
+            Assert.False(contatoRsult.IsValid);
+            Assert.Contains("E-mail inválido.", contatoRsult.Errors.Select(e => e.ErrorMessage));
+        }
+
+
+
+        private Contato ObterContatoMock(string nome, string email)
+        {
+            Telefone telefone = new Telefone("13", "997843424");
+            Contato contato = new Contato(nome, email, telefone);
+            return contato; 
+        }
+    }
+}
--- src/Tech.Challenge.Grupo27.Domain/Models/ContatoAggregate/ContatoValidator.cs
+++ src/Tech.Challenge.Grupo27.Domain/Models/ContatoAggregate/ContatoValidator.cs
@@ -2,7 +2,7 @@
 
 namespace Tech.Challenge.Grupo27.Domain.Models.ContatoAggregate
 {
-    internal class ContatoValidator : AbstractValidator<Contato>
+    public class ContatoValidator : AbstractValidator<Contato>
     {
         public ContatoValidator()
         {
@@ -19,10 +19,10 @@ namespace Tech.Challenge.Grupo27.Domain.Models.ContatoAggregate
                 .WithMessage("E-mail é obrigatório.")
                 .WithErrorCode("EMAIL_OBRIGATORIO")
                 .EmailAddress()
-                .WithMessage("O e-mail não é válido")
-                .WithErrorCode("EMAIL_INVALIDO");                 
+                .WithMessage("E-mail inválido.")
+                .WithErrorCode("EMAIL_INVALIDO");
 
-            RuleFor(x=> x.Telefone).SetValidator(new TelefoneValidator());
+            RuleFor(x => x.Telefone).SetValidator(new TelefoneValidator());
 
         }
     }
