1749c40f6e94f9f9ebd56e5ea7d589b718039a0e
Tiago <rm353730@fiap.com.br>
2024-07-25 14:07:34 -0300

    ajustando dockerfile

--- local/compose/sqlserver/run-initialization.sh
+++ local/compose/sqlserver/run-initialization.sh
@@ -1,3 +1,3 @@
 sleep 40s
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P @d5^5kUPjhFd-74 -d master -i create_database.sql 
+/opt/mssql-tools/bin/sqlcmd -S nossobancopostech.database.windows.net Authentication=Active Directory -U RM353294@fiap.com.br -P @d5^5kUPjhFd-74 -d master -i create_database.sql 
